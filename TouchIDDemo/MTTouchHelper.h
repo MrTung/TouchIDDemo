f29cf170caf3d36d4877d56f0799e81084dc00a2
dongxuwei <287178790@qq.com>
2016-12-07 11:17:32 +0800

    添加图片

---
+++ TouchIDDemo/MTTouchHelper.h
@@ -0,0 +1,16 @@
+//
+//  MTTouchHelper.h
+//  TouchIDDemo
+//
+//  Created by 董徐维 on 2016/12/7.
+//  Copyright © 2016年 董徐维. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface MTTouchHelper : NSObject
+
++(BOOL)validateByTouchID;
+
+@end
